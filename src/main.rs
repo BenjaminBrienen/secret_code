7ed34da2945cd5ba872fcac26f4eda0abc04d844
BenjaminBrienen <Benjamin.Brienen@outlook.com>
2023-03-17 22:05:20 -0400

    include_bytes

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,19 @@
 #![feature(slice_group_by)]
 
-use std::{
-	error::Error,
-	fs::File,
-	io::Read,
-};
+use std::error::Error;
 
 type ProgramError = Box<dyn Error>;
-type Memory = Vec<u8>;
+type Memory = [u8];
 
 fn main() -> Result<(), ProgramError>
 {
-	let file_bytes = read_file_bytes("input.txt")?;
+	let file_bytes = include_bytes!("../input.txt");
 	let secret = parse_puzzle_into_secret(file_bytes)?;
 	println!("{secret}");
 	Ok(())
 }
 
-fn parse_puzzle_into_secret(file_bytes: Memory) -> Result<String, ProgramError>
+fn parse_puzzle_into_secret(file_bytes: &Memory) -> Result<String, ProgramError>
 {
 	let byte_is_ascii_0_or_1 = |file_byte: &u8, _: &u8| byte_to_bit(*file_byte).is_some();
 	let parsed = file_bytes
@@ -26,13 +22,6 @@ fn parse_puzzle_into_secret(file_bytes: Memory) -> Result<String, ProgramError>
 		.collect();
 	let result = String::from_utf8(parsed)?;
 	Ok(result)
-}
-
-fn read_file_bytes(file_name: &str) -> Result<Memory, ProgramError>
-{
-	let mut buffer = Vec::new();
-	File::open(file_name)?.read_to_end(&mut buffer)?;
-	Ok(buffer)
 }
 
 fn parse_plaintext_byte(plaintext_byte: &[u8]) -> u8
