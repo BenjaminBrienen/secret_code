e4c5400b823b18de2902608f38ddad8f01cd3fae
BenjaminBrienen <Benjamin.Brienen@outlook.com>
2023-03-17 21:08:54 -0400

    remove braces

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ fn main()
 {
 	let mut buffer = Vec::new();
 	std::io::Read::read_to_end(&mut std::fs::File::open("input.txt").unwrap(), &mut buffer).unwrap();
-	println!("{}", String::from_utf8(buffer.group_by(|byte, _| byte_to_bit(*byte).is_some()).map(|plaintext_byte| { plaintext_byte.iter().filter(|byte| byte_to_bit(**byte).is_some()).rev().enumerate().fold(0, |acc, index_byte| acc + (byte_to_bit(*index_byte.1).unwrap_or(0) << index_byte.0))}).collect()).unwrap());
+	println!("{}", String::from_utf8(buffer.group_by(|byte, _| byte_to_bit(*byte).is_some()).map(|plaintext_byte| plaintext_byte.iter().filter(|byte| byte_to_bit(**byte).is_some()).rev().enumerate().fold(0, |acc, index_byte| acc + (byte_to_bit(*index_byte.1).unwrap_or(0) << index_byte.0))).collect()).unwrap());
 }
 fn byte_to_bit(byte: u8) -> Option<u8>
 {
